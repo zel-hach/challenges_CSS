ff9219bbd731e325ae5628a909ffe5abf6ca01dd
zel-hach <zineb.ell.zee@gamil.com>
2024-05-24 20:42:01 +0100

    backend of my app chat

--- chat_app/chat_app_front/src/Components/Login/Register.tsx
+++ chat_app/chat_app_front/src/Components/Login/Register.tsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Login from './login';
 
 export default function Register(props:any) {
+  const [userName,setUserName] = useState("");
+  const [password,setPassword] = useState("");
+  const [email,setEmail] = useState("");
+  const [phone,setPhone] = useState("");
     const go_To_login = () =>{
         props.setLogin(true);
     }
+
+    const Register = async() => {
+        const response = await fetch("http:localhost:3000/users/register",{
+            method:"POST",
+            headers:{'Content-type':'application/json'},
+            body:JSON.stringify({
+              username:userName,
+              email,
+              phone,
+              password
+            }) 
+        })
+    }
+
   return (
     <div className='flex flex-col items-center gap-7 w-full m-12'>
+      <form className='w-full flex flex-col items-center gap-7' onSubmit={Register}>
     <div className='w-full flex flex-col '>
       <label className='invisible'>Username</label>
-      <input type='text' placeholder='Username' className='rounded p-2 bg-inherit border-b-2 outline-none'></input>
+      <input type='text' placeholder='Username' className='rounded p-2 bg-inherit border-b-2 outline-none' onChange={(e)=>setUserName(e.target.value)}></input>
     </div>
     <div className=' w-full flex flex-col '>
       <label className='invisible'>Email</label>
-      <input type='email' placeholder='Email' className='rounded p-2 bg-inherit border-b-2 outline-none'></input>
+      <input type='email' placeholder='Email' className='rounded p-2 bg-inherit border-b-2 outline-none' onChange={(e)=>setEmail(e.target.value)}></input>
     </div>
     <div className='w-full flex flex-col'>
       <label className='invisible'>Phone</label>
-      <input type='phone' placeholder='Phone' className='rounded p-2 bg-inherit border-b-2 outline-none'></input>
+      <input type='phone' placeholder='Phone' className='rounded p-2 bg-inherit border-b-2 outline-none' onChange={(e)=>setPhone(e.target.value)}></input>
     </div>
     <div className='w-full flex flex-col'>
         <label className='invisible'>Password</label>
-        <input type='password' placeholder='Password' className='rounded p-2 bg-inherit border-b-2 outline-none'></input>
+        <input type='password' placeholder='Password' className='rounded p-2 bg-inherit border-b-2 outline-none' onChange={(e)=>setPassword(e.target.value)}></input>
     </div>
     <div className='w-full flex justify-center'>
       <button className='w-full p-3 bg-zinc-950 rounded' >Log in</button>
@@ -28,6 +48,7 @@ export default function Register(props:any) {
     <div>
       <p>Have an account ? <span className='text-slate-950 font-bold cursor-pointer' onClick={go_To_login}>log in</span></p>
     </div>
+      </form>
   </div>
   )
 }
--- chat_app/chat_app_front/src/Components/Login/login.tsx
+++ chat_app/chat_app_front/src/Components/Login/login.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login(props:any) {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
     const go_to_Register = () => {
         props.setLogin(false)
     }
+
+    const Login = async () =>{
+        const response = await fetch("http://localhost:3000/users/login",{
+            method:"POST",
+            headers:{'Content-type':'application/json'},
+            body:JSON.stringify({
+                email,
+                password,
+            })
+        })
+    }
     return (
-        <div className='flex flex-col items-center m-12 gap-7 w-full'>
+        <div className='flex flex-col items-center m-12 w-full'>
+        <form className='w-full flex flex-col items-center gap-7' onSubmit={Login}>
             <div className='w-full flex flex-col'>
-                <label className='invisible'>Username</label>
-                <input type='text' placeholder='Username' className='rounded p-2 bg-inherit border-b-2 outline-none'></input>
+                <label className='invisible'>Email</label>
+                <input type='text' placeholder='Email' className='rounded p-2 bg-inherit border-b-2 outline-none' onChange={(e)=>setEmail(e.target.value)}></input>
             </div>
             <div className='w-full flex flex-col'>
                 <label className='invisible'>Password</label>
-                <input type='password' placeholder='Password' className='rounded p-2 bg-inherit border-b-2 outline-none'></input>
+                <input type='password' placeholder='Password' className='rounded p-2 bg-inherit border-b-2 outline-none' onChange={(e)=>setPassword(e.target.value)}></input>
             </div>
             <div className='w-full flex justify-center'>
                 <button className='p-3 bg-zinc-950 rounded w-full' >Log in</button>
@@ -20,6 +34,7 @@ export default function Login(props:any) {
             <div className='w-full'>
                 <p>create new account <span className='text-slate-950 font-bold cursor-pointer' onClick={go_to_Register}>sign up</span></p>
             </div>
+        </form>
         </div>
     )
 }
